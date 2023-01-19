00562341e76b7ed685c4ebd3994cdbc8e812426d
shiwani <shiwanipaunikar1@gmail.com>
2023-01-19 11:06:51 -0800

    Added:Tic Tac Toe simulation program

---
+++ src/com/bridgelabz/tictactoe/TicTacToe.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.tictactoe;
+
+public class TicTacToe {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Tic Tac Toe simulation program");
+    }
+}
