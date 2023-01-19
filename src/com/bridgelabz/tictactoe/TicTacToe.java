ee4ce01bf73761ee5865853e36048ed03d3468f0
shiwani <shiwanipaunikar1@gmail.com>
2023-01-19 11:19:01 -0800

    Added:Display the empty board

--- src/com/bridgelabz/tictactoe/TicTacToe.java
+++ src/com/bridgelabz/tictactoe/TicTacToe.java
@@ -11,6 +11,7 @@ public class TicTacToe {
     public static void main(String[] args) {
         createEmptyBoard();
         chooseLetter();
+        showBoard();
     }
 
     /**
@@ -36,5 +37,18 @@ public class TicTacToe {
         userLetter = scanner.next().toUpperCase().charAt(0);
         computerLetter = (userLetter == 'X') ? 'O' : 'X';
     }
+    /**
+     * UC3
+     * Method Name: showBoard
+     * method to display current board
+     */
+    private static void showBoard() {
+        System.out.println(board[1] + " | " + board[2] + " | " + board[3]);
+        System.out.println("----------");
+        System.out.println(board[4] + " | " + board[5] + " | " + board[6]);
+        System.out.println("----------");
+        System.out.println(board[7] + " | " + board[8] + " | " + board[9]);
+    }
+
 
 }
