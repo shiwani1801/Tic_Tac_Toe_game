80e4e5fc2d56a72ce23c8167c7a3803f0f9f5f56
shiwani <shiwanipaunikar1@gmail.com>
2023-01-19 11:10:59 -0800

    Added:Create a empty board

--- src/com/bridgelabz/tictactoe/TicTacToe.java
+++ src/com/bridgelabz/tictactoe/TicTacToe.java
@@ -1,7 +1,26 @@
 package com.bridgelabz.tictactoe;
 
 public class TicTacToe {
+    static char[] board = new char[10];
+
     public static void main(String[] args) {
-        System.out.println("Welcome to Tic Tac Toe simulation program");
+        createEmptyBoard();
+
+    }
+
+    /**
+     * UC1
+     * Method Name: createEmptyBoard
+     * Static method to create empty board
+     * It takes board array indexes from 1 to 9 using for loop
+     */
+    private static void createEmptyBoard() {
+        for (int index = 1; index < board.length; index++) {
+            board[index] = ' ';
+        }
     }
 }
+
+
+
+
